8cd1e9fb7e408777d2744b38ca94da1be8097f6a
Nicholas McFarlane <nicholas.mcfarlane@gmail.com>
2026-09-21 19:47:30 +0200

    Enhance Interface with menu-driven user interaction, implement member registration logic in Library, and introduce dynamic member storage expansion.

--- src/main/java/org/example/Interface.java
+++ src/main/java/org/example/Interface.java
@@ -1,15 +1,34 @@
 package org.example;
 
-public interface Interface {
+import java.util.Scanner;
+
+public class Interface {
     static void main(){
-        IO.print("Welcome to the archives of Minas Tirith.");
-        IO.print("Enter 1 if you are an existing member: ");
-        if (Integer.parseInt(IO.readln()) == 1){
-            IO.print("Enter your ID number: ");
-            int idNumber = Integer.parseInt(IO.readln());
-            Member member = new Member(name, idNumber);
-            IO.print("Welcome back, " + member.getName() + "!");
+        Library library = new Library();
+        Scanner scanner = new Scanner(IO.readln());
+        boolean running = true;
+
+        IO.println("Welcome to the archives of Minas Tirith.");
+
+        while (running){
+            IO.println("\n--- Main Menu ---");
+            IO.println("1. Add a book.");
+            IO.println("2. Register member.");
+            IO.println("3. Loan book.");
+            IO.println("4. Return loaned book.");
+            IO.println("5. Search for a book.");
+            IO.println("6. List of all books.");
+            IO.println("7. Exit.");
+            IO.println("Please choose an option (1-7): ");
+
+            int choice = Integer.parseInt(IO.readln());
+
+            switch (choice){
+                case 1:
+                    IO.println("Enter the title of the book: ");
+                    //Todo: prompt for bbook details and create library.addBook()
+            }
         }
-        IO.println("Enter 2 if you are a new member: ");
+
     }
 }
--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,6 +1,41 @@
 package org.example;
 
 public class Library {
+    private Member[] members = new Member[10];
+    private int memberCount = 0;
+
+    public void registerNewMember(String name, int idNumber){
+        if(memberCount >= members.length){
+            Member[] members2 = new Member[members.length * 2];
+            for(int i = 0; i < members.length; i++){
+                members2[i] = members[i];
+            }
+            members = members2;
+        }
+        if (idNumber < 1000 || idNumber > 9999){
+            IO.println("Invalid ID number. Please choose a 4-digit pin.");
+            return;
+        }
+        for(int i = 0; i < memberCount; i++){
+            if(members[i].getIdNumber() == idNumber){
+                IO.println("Member with ID number " + idNumber + " already exists. Please choose a different ID number.");
+                return;
+            }
+        }
+      Member newMember = new Member(name, idNumber);
+      members[memberCount] = newMember;
+      memberCount++;
+
+      IO.println("Member " + name + " with ID number " + idNumber + " has been registered.");
+    }
+    public Member findMemberById(int idNumber){
+      for(int i = 0; i < memberCount; i++){
+        if(members[i].getIdNumber() == idNumber){
+          return members[i];
+        }
+      }
+      return null;
+    }
 }
 
 record book (String isbn, String title, String author){}
